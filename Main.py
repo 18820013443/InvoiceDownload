05e0b22d0b40f5c5295f38359a8e8f5d7bec6c74
kk <331479961@qq.com>
2022-11-17 10:28:52 +0800

    wiating for test

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ from Excel import Data, AppExcel
 from Invoice import InvoiceOperations
 from ClipPic import Pic
 import os
+import datetime
 
 
 class Main:
@@ -19,14 +20,17 @@ class Main:
             for index, row in df.iterrows():
                 strInvoiceNum = row['发票号码'].strip()
                 isFirstTime = True if index == 0 else False
-                dicDateFrom = self.GetDicDate(row[''].strip())
-                dicDateTo = self.GetDicDate(row[''].strip())
+                dicDateFrom = self.GetDicDate(True, row['开票日期'].strip())
+                dicDateTo = self.GetDicDate(False)
                 strImgInputPath = InvoiceOperations(dicDateFrom, dicDateTo, strInvoiceNum, isFirstTime)
                 screenshotsList.append(strImgInputPath)
+            
+            objExcel = AppExcel()
+            objExcel.WriteToExcel(strFilePath, sheetName, df)
         
         for strImgInputPath in screenshotsList:
             strOutputImgName = os.path.basename(strOutputImgPath)
-            strOutputImgPath = os.path.join(os.getcwd(), strOutputImgName)
+            strOutputImgPath = os.path.join(os.getcwd(), r'ClipImgs\%s'%strOutputImgName)
             objPic.ClipImg(strInputImgPath, strOutputImgPath)
                 
 
@@ -40,9 +44,41 @@ class Main:
                 outputList.append(strFilePath)
         return outputList
 
-    def GetDicDate(self, date):
-        pass
-        
+    def GetDicDate(self, isFromDate, date=''):
+        if isFromDate:
+            strList = date.split('-')
+            dicDateFrom = {
+                'elXYear': 150,
+                'year': '2022',
+                'elYYear': 94,
+                'elXMonth': 185,
+                'month': '10',
+                'elYMonth': 94,
+                'elXDay': 219,
+                'day': '01',
+                'exYDay': 94
+            } # Input Params
+            dicDateFrom['year'] = strList[0]
+            dicDateFrom['month'] = strList[1]
+            dicDateFrom['day'] = strList[2].split(' ')[0]
+            return dicDateFrom
+        else:
+            date = datetime.datetime.now()
+            dicDateTo = {
+                'elXYear': 150,
+                'year': '2022',
+                'elYYear': 156,
+                'elXMonth': 185,
+                'month': '11',
+                'elYMonth': 156,
+                'elXDay':219,
+                'day': '15',
+                'exYDay': 156
+            } # Input Params
+            dicDateTo['year'] = date.year
+            dicDateTo['month'] = date.month
+            dicDateTo['day'] = date.day
+            return dicDateTo
 
 if __name__ == '__main__':
     objMain = Main()
